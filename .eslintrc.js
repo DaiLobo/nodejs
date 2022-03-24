959ccffd8ef3da3d6a8927b9dd0f1f091b974b90
Diana <diana45637@live.com>
2022-03-23 22:19:17 -0300

    Inicializando o projeto movie

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+const eslintConfig = require("@monorepo/eslint-config");
+
+modeule.exports = eslintConfig; //para usar em todo projeto e não somente na pasta package
---
+++ backend/packages/eslint-config/.eslintrc.js
@@ -0,0 +1,15 @@
+export default {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
